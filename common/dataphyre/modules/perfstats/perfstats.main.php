2e3b086458785d4c401efce3b77235a14fe122d7
Jérémie Fréreault <jerefrereault@hotmail.com>
2024-11-16 17:01:47 -0500

    Update perfstats.main.php

--- common/dataphyre/modules/perfstats/perfstats.main.php
+++ common/dataphyre/modules/perfstats/perfstats.main.php
@@ -23,73 +23,89 @@ if(file_exists($filepath=$rootpath['common_dataphyre']."config/perfstats.php")){
 if(file_exists($filepath=$rootpath['dataphyre']."config/perfstats.php")){
 	require_once($filepath);
 }
-if(!isset($configurations['dataphyre']['perfstats'])){
-	//core::unavailable("MOD_PERFSTATS_NO_CONFIG", "safemode");
-}
 
-if(core::get_config("dataphyre/perfstats/get_rps_on_load")===true){
+if($configurations['dataphyre']['perfstats']['get_rps_on_load']===true){
 	perfstats::get_rps();
 }
 
+/**
+ * Class perfstats
+ *
+ * This class provides functionality for calculating and caching requests per second (RPS) statistics.
+ */
 class perfstats{
 	
+    /**
+     * @var array $rps_samples Cache for RPS values, indexed by sampling rate.
+     */
 	private static $rps_samples=[];
 	
-	public static function get_rps($sampling_rate=60){
-		tracelog(__FILE__, __LINE__, __CLASS__, __FUNCTION__, $S = null, $T = 'function_call', $A = func_get_args()); // Log the function call
-		if (null !== $early_return = core::dialback("CALL_PERFSTATS_GET_RPS", ...func_get_args())) {
+    /**
+     * Get the requests per second (RPS) value for a given sampling rate.
+     *
+     * This method calculates RPS based on cached samples or updates the cache with new samples.
+     * It creates and maintains a directory structure for caching and cleans up outdated files.
+     *
+     * @param int $sampling_rate The interval in seconds for sampling RPS (default is 60 seconds).
+     * @return float|false The calculated RPS value, or false on failure.
+     */
+	public static function get_rps(int $sampling_rate=60){
+		tracelog(__FILE__, __LINE__, __CLASS__, __FUNCTION__, $S=null, $T='function_call', $A=func_get_args()); // Log the function call
+		if(null!==$early_return=core::dialback("CALL_PERFSTATS_GET_RPS", ...func_get_args())){
 			return $early_return;
 		}
 		global $rootpath;
-		$cachePath = $rootpath['dataphyre'] . 'cache/perfstats/rps_cache/';
-		// Ensure the cache directory exists
-		if (!is_dir($cachePath)) {
-			// Using recursive directory creation
-			if (!mkdir($cachePath, 0777, true) && !is_dir($cachePath)) {
-				// Log error if unable to create the directory
-				tracelog(__FILE__, __LINE__, __CLASS__, __FUNCTION__, $S = "Error creating directory");
+		$cache_cath=$rootpath['dataphyre'].'cache/perfstats/rps_cache/';
+		if(!is_dir($cache_cath)){
+			if(!mkdir($cache_cath, 0777, true) && !is_dir($cache_cath)){
+				tracelog(__FILE__, __LINE__, __CLASS__, __FUNCTION__, $S="Error creating directory");
 				return false;
 			}
 		}
-		if (!isset(perfstats::$rps_samples[$sampling_rate])) {
-			$files = scandir($cachePath);
-			if ($files === false) {
-				// Log error if unable to scan directory
-				tracelog(__FILE__, __LINE__, __CLASS__, __FUNCTION__, $S = "Error scanning directory");
+		if(!isset(perfstats::$rps_samples[$sampling_rate])){
+			$files=scandir($cache_cath);
+			if($files===false){
+				tracelog(__FILE__, __LINE__, __CLASS__, __FUNCTION__, $S="Error scanning directory");
 				return false;
 			}
-			$files = array_diff($files, ['.', '..']);
-			$samples = [];
-			$i = 5;
-			foreach ($files as $filename) {
-				$filePath = $cachePath . $filename;
-				if ((int)$filename < strtotime("-5 minute")) {
-					unlink($filePath);
-				} else {
+			$files=array_diff($files, ['.', '..']);
+			$samples=[];
+			$i=5;
+			foreach($files as $filename){
+				$file_path=$cache_cath.$filename;
+				if((int)$filename<strtotime("-5 minute")){
+					unlink($file_path);
+				}
+				else
+				{
 					$i++;
-					if ($i >= 1) {
-						$i = 0;
-						if (is_file($filePath)) {
-							$samples[] = file_get_contents($filePath);
+					if($i>=1){
+						$i=0;
+						if(is_file($file_path)){
+							$samples[]=file_get_contents($file_path);
 						}
 					}
 				}
 			}
-			if (strtotime("now") % $sampling_rate == 0) {
-				$current_file = $cachePath . strtotime("now");
-				if (!is_file($current_file)) {
+			if(strtotime("now")%$sampling_rate==0){
+				$current_file=$cache_cath.strtotime("now");
+				if(!is_file($current_file)){
 					file_put_contents($current_file, 1);
-				} else {
-					file_put_contents($current_file, (int)file_get_contents($current_file) + 1);
+				}
+				else
+				{
+					file_put_contents($current_file, (int)file_get_contents($current_file)+1);
 				}
 			}
-			if (!empty($samples)) {
-				perfstats::$rps_samples[$sampling_rate] = round(array_sum($samples) / count($samples), 2);
-			} else {
-				perfstats::$rps_samples[$sampling_rate] = 0;
+			if(!empty($samples)){
+				perfstats::$rps_samples[$sampling_rate]=round(array_sum($samples)/count($samples), 2);
+			}
+			else
+			{
+				perfstats::$rps_samples[$sampling_rate]=0;
 			}
 		}
-		tracelog(__FILE__, __LINE__, __CLASS__, __FUNCTION__, $S = "Done");
+		tracelog(__FILE__, __LINE__, __CLASS__, __FUNCTION__, $S="Done");
 		return perfstats::$rps_samples[$sampling_rate];
 	}
 
