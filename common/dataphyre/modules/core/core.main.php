16dc1afe4be17dca27370093ed1874ab6623762a
Jérémie Fréreault <jerefrereault@hotmail.com>
2024-12-12 11:38:55 -0500

    Update core.main.php

--- common/dataphyre/modules/core/core.main.php
+++ common/dataphyre/modules/core/core.main.php
@@ -141,32 +141,26 @@ if(RUN_MODE!=='diagnostic'){
 	if($mod=dp_module_present('contingency')){
 		require($mod[0]);
 		\dataphyre\contingency::set_handler();
-		function attempt($a=null){ 
-			return \dataphyre\contingency::attempt($a);
-		}
-		function end_attempt(){ 
-			return \dataphyre\contingency::end_attempt();
-		}
-		function define_optional($a=null,$b=null){ 
-			return \dataphyre\contingency::define_optional($a,$b);
-		}
+		function attempt($a=null){ return \dataphyre\contingency::attempt($a);}
+		function end_attempt(){return \dataphyre\contingency::end_attempt();}
+		function define_optional($a=null,$b=null){return \dataphyre\contingency::define_optional($a,$b);}
 	}
 	else
 	{
-		function attempt($a){ 
-			return true;
-		}
-		function end_attempt(){ 
-			return false;
-		}
-		function define_optional($a, $b){
-			return false;
-		}
+		function attempt($a){return true;}
+		function end_attempt(){return false;}
+		function define_optional($a, $b){return false;}
 	}
 
 	if($mod=dp_module_present('sql')){
 		require($mod[0]);
 		new \dataphyre\sql();
+		function sql_count($a=null,$b=null,$c=null, $d=null, $e=null, $f=null){return dataphyre\sql::db_count($a,$b,$c,$d,$e,$f);}
+		function sql_select($a=null,$b=null,$c=null,$d=null,$e=null,$f=null,$g=null,$h=null){return dataphyre\sql::db_select($a,$b,$c,$d,$e,$f,$g,$h);}
+		function sql_delete($a=null,$b=null,$c=null,$d=null,$e=null,$f=null){return dataphyre\sql::db_delete($a,$b,$c,$d,$e,$f);}
+		function sql_update($a=null,$b=null,$c=null,$d=null,$e=null,$f=null,$g=null){return dataphyre\sql::db_update($a,$b,$c,$d,$e,$f,$g);}
+		function sql_insert($a=null,$b=null,$c=null,$d=null,$e=null,$f=null){return dataphyre\sql::db_insert($a,$b,$c,$d,$e,$f);}
+		function sql_query($a=null,$b=null,$c=null,$d=null,$e=null,$f=null, $g=null, $h=null){return dataphyre\sql::db_query($a,$b,$c,$d,$e,$f,$g,$h);}
 	}
 
 	if(RUN_MODE==='request'){
